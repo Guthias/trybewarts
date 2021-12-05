fdde444344854a6699abeb3836149e223ecb6e4c
guthias <gmgustavom@gmail.com>
2021-12-05 14:05:19 -0300

    Pegando dados do formulario

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const agreeCheckbox = document.getElementById('agreement');
 const buttonSubmit = document.getElementById('submit-btn');
 const textarea = document.getElementById('textarea');
 const charCounter = document.getElementById('counter');
+const firstNameInput = document.getElementById('input-name');
+const lastNameInput = document.getElementById('input-lastname');
+const emailInput = document.getElementById('input-email');
+const houseSelect = document.getElementById('house');
 
 function checkLogin(event) {
   event.preventDefault();
@@ -20,6 +24,39 @@ function checkLogin(event) {
 
 function changeSubmitState() {
   buttonSubmit.disabled = !buttonSubmit.disabled;
+}
+
+function getSelectedRadio(name) {
+  const selected = document.querySelector(`input[name=${name}]:checked`);
+  if (!selected) {
+    return;
+  }
+  return selected.value;
+}
+
+function getSelectedCheckboxes(name) {
+  const checkboxes = document.querySelector(`input[name=${name}]:checked`);
+  const values = [];
+
+  if (!checkboxes) {
+    return;
+  }
+  for (let i = 0; i < checkboxes.length; i += 1) {
+    values.push(checkboxes[i].value);
+  }
+
+  return values;
+}
+
+function getFormData() {
+  return {
+    name: `${firstNameInput.value} + ${lastNameInput.value}`,
+    email: emailInput.value,
+    family: houseSelect.value,
+    house: getSelectedRadio('family'),
+    subjects: getSelectedCheckboxes('subject'),
+    rate: getSelectedRadio('rate'),
+    obs: textarea.value };
 }
 
 buttonLogin.addEventListener('click', checkLogin);
